356b3075b3464826c02963f78956441c5a9bdd55
Odai <52354317+OdaiAlzoubi@users.noreply.github.com>
2022-01-11 21:27:02 +0200

    Create Notification

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -257,6 +257,10 @@
                 <li class="nav-item">
                     <a class="nav-link {{ request()->is('notification*') ? 'active' : '' }} "
                         href="{{ route('notification') }}">
+                        <span  id='odai' class="position-absolute top-0 translate-middle p-2 bg-danger border border-light rounded-circle d-none"
+                        style="right: 90% !important;">
+                            <span class="visually-hidden">New alerts</span>
+                        </span>
                         <div
                             class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                             <svg width="12px" height="12px" viewBox="0 0 40 40" version="1.1"
@@ -926,5 +930,23 @@
 
 </body>
 
+<script type="text/javascript">
+
+
+
+
+    const Notification = 5 ;
+    const N = document.getElementById('odai');
+
+    if (Notification > 0) {
+        N.className += 'd-none';
+
+    }
+    else{
+        N = N.className.replace('d-none','');
+    }
+    settimeout(realTime , 500);
+
+</script>
 
 </html>
